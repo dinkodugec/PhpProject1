c7fd858eb38944d950f1f533dca8cb960c59840e
Dinko <dinko.dugec@gmail.com>
2023-03-28 00:32:06 +0200

    composer autoload

--- public/index.php
+++ public/index.php
@@ -4,12 +4,22 @@
 use app\Models\Flight;
 
 
-spl_autoload_register(function($class){
+/* spl_autoload_register(function($class){
 
    $path =__DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-   require $path;
-});
+        if(file_exists($path)){  //psr-4 - can not raise error or waning
+            require $path;
+        } 
+}); */
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$flight = new Flight();
+/* Composer  */
+
+require_once '../vendor/autoload.php';
+
+$id = new \Ramsey\Uuid\UuidFactory();
+echo $id->uuid4();
+ 
+$flight = new Flight;
 echo $flight->hello();
