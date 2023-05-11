5fc18f40cc15e75865e63c0bc45547815555c57b
Dinko <dinko.dugec@gmail.com>
2023-05-11 21:30:28 +0200

    Variable Storage & Object Comparison

--- public/index.php
+++ public/index.php
@@ -33,88 +33,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once '../vendor/autoload.php';
 
 
-$transanction = new Transaction(25);
 
 
- $transanction->process();
+ $pack1 = new Package(3, 'ship'); 
+ $pack2= new Package(3,'ship');
 
- $pack1 = new Package(2,3 , 'ship'); 
- $packPap = new PaperPackage(3.4 , 'ship');
- echo $packPap->get_Pack();
- echo $packPap->deliveryPack();
+ $pack3 = $pack1;
+ echo '$pack1' === '$pack3'. PHP_EOL;
+var_dump('$pack1' === '$pack3');  //return true, it references to the same point in memory; ZEND container, similiar like assign by reference in variable
 
- function typeHintingOfObject( PaperPackage $packPap)
- {
-    $packPap->deliveryPack();
- }
+echo '$pack1' == '$pack2' . PHP_EOL;
+var_dump('$pack1' == '$pack2'); //comparision operator
+//two objects will be equal if they are objects of two same classes, with same properties and values for them
 
- echo typeHintingOfObject($packPap);
-
- $gift = new FancyGiftPackage($packPap );
- echo $gift->deliveryPack(); 
-
- $or = new Order(new ShopOrder());
- echo $or->order->getOrder();
-
-
-$LatteMaker = new LatteMaker();
-$LatteMaker-> makeLatte(); 
-
-$cappuccinoMaker = new CappuccinoMaker();
-$cappuccinoMaker-> makeCappuccino();
-
-$allinone = new AllInnOneCoffeeMaker;
-$allinone->makeCoffe(); // extending from Coffe-maker class
-$allinone->makeCappuccino(); //from traits CappuccinoTrait
-
-
-//traits are just like COPY and PASTE;  it just take  code from traits and use in class which use trait
-
-/* it is able to redefine method that is defined in a trait so method 
-that is called from class that use trait has precedence
-*/
-
-/* When use a static properties in inhertance, static propertires is shared and when we change a value it is affected to orginal class
-but in traits, static properties is not shared and every class that use traits has his own version   */
-
-/* VERY SIMPLE - USE TRAITS TO CODE REPETITION */
-/* Main usecases of using anonymus classes is testing */
-/* $obj = new class(1,2,3) { //anonymus classes- can use interface, inhertiance, traits etc
-   public function __construct(public $x, public int $y, public int $z)
-   {
-      
-   }
-};
-
-var_dump($obj); */
-
-$obj = new classA (1, 2);
-
-var_dump($obj->bar(2));
-
-class classA 
-{
-   public function __construct(public $x, public int $y)
-   {
-      
-   }
-
-   public function foo()
-   {
-      return 'foo';
-   }
-
-   public function bar(): object
-   {
-      return new class {
-         public function __construct()
-         {
-
-         }
-       
-      };
-   }
-}
+echo '$pack1' === '$pack2'. PHP_EOL;
+var_dump('$pack1' === '$pack2'); //identiy operator
+// will be the same if they point to same instance from same class
 
 
 
